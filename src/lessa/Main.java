f132c685240f1a7cf6818c96d4464b1a6fdb85fa
giddens9527 <liujiaqi0610@gmail.com>
2015-04-18 12:48:20 -0400

    add main.java

---
+++ src/lessa/Main.java
@@ -0,0 +1,14 @@
+Package lessa;
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        ExprLexer lexer = new ExprLexer(new ANTLRFileStream("?"));
+        ExprParser parser = new ExprParser(new CommonTokenStream(lexer));
+        ParseTree tree = parser.parse();
+        EvalVisitor visitor = new EvalVisitor();
+        visitor.visit(tree);
+    }
+}
