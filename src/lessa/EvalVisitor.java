1c0ba4d000758b1c1841be7c594f18a0dbfd7a93
gmf <mg3534@columbia.edu>
2015-04-22 23:44:27 -0400

    begin implementing selection-stmt

--- src/lessa/EvalVisitor.java
+++ src/lessa/EvalVisitor.java
@@ -9,34 +9,45 @@ import envir.Gen;
 
 
 public class EvalVisitor extends ExprBaseVisitor<Integer> {
-		Map<String, Integer> memory = new HashMap<String, Integer>();
+  Map<String, Integer> memory = new HashMap<String, Integer>();
 
-		public EvalVisitor() throws FileNotFoundException, UnsupportedEncodingException {
-			//writer = new PrintWriter("out.py", "UTF-8");
-		}
+  public EvalVisitor() throws FileNotFoundException, UnsupportedEncodingException {
+    //writer = new PrintWriter("out.py", "UTF-8");
+  }
 
 
-		/** print_stmt: PRINT STRING ;*/
-		@Override 
-		public Integer visitPrint_stmt(ExprParser.Print_stmtContext ctx) {
-				String print = ctx.PRINT().getText();
-				String value = ctx.STRING().getText();
-				
-				try {
-				    System.out.println("-----------------generating code-----------------\n");
-					Writer w = new FileWriter(Envir.exeFileName, false);
-					w.write(print);
-					w.write(" ");
-					w.write(value);
-					//System.out.println(print+" "+value);
-					w.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				return 0;
-		}
+  /** print_stmt: PRINT STRING ;*/
+  @Override 
+  public Integer visitPrint_stmt(ExprParser.Print_stmtContext ctx) {
+    String print = ctx.PRINT().getText();
+    String value = ctx.STRING().getText();
+
+    try {
+      //System.out.println("-----------------generating code-----------------\n");
+      Writer w = new FileWriter(Envir.exeFileName, false);
+      w.write(print);
+      w.write(" ");
+      w.write(value);
+      //System.out.println(print+" "+value);
+      w.close();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    return 0;
+  }
+  @Override 
+  public Integer visitExpr_stmt(ExprParser.Expr_stmtContext ctx) { 
+    return visitChildren(ctx); 
+  }
+  @Override 
+  public Integer visitSelection_stmt(ExprParser.Selection_stmtContext ctx){
+    System.out.println("start:"+ctx.getRuleContext().getText());
+    
+    return visitChildren(ctx);
+		  
+  }
 
 }
 
