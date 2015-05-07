ecda9cb7e874dd5e2c058f5739d8ffe34a7dffd9
gmf <mg3534@columbia.edu>
2015-05-07 18:26:08 -0400

    add lessa exception handler

---
+++ src/envir/LessaError.java
@@ -0,0 +1,11 @@
+package envir;
+
+public class LessaError {
+  public String type;
+  public String codeNO;
+  
+  public LessaError(String content, String no){
+    this.type = content;
+    this.codeNO = no;
+  }
+}
